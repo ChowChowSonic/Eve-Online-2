7b3429a62eb8b139e11a844faf7de5dbb8b279b6
ChowChowSonic <joeyant1367@gmail.com>
2021-01-14 19:45:54 -0500

    Rebuilt Project for local repository
    Dependencies weren't being recognised on my new setup so I rebuilt the entire project.

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import my.gdx.game.EveOnline2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EveOnline2(), config);
+	}
+}
